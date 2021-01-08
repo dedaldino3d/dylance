cb8f37e1b4553192ecafb7ac76c9015ab7caf898
Dedaldino3D <dedaldinoantonio7@gmail.com>
2021-01-08 16:15:39 +0100

    Add users urls

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.ListFreelancers.as_view(), name="list_frelancers"),
+    path('<str:username>', views.UserProfileDetail, name="profile_detail"),
+    path("<str:username>/config", views.UpdateProfileView, name="update_profile"),
+    path('become_freelancer/', views.become_freelancer, name="become_freelancer"),
+    path('un_become_freelancer/', views.un_become_freelancer, name="un_become_freelancer"),
+]
