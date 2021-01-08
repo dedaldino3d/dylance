900b0a6ca82dd46e06f52d290328e1f2b8b519ba
Dedaldino3D <dedaldinoantonio7@gmail.com>
2021-01-08 15:54:53 +0100

    Create views for users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import UserSerializer, ProfileSerializer
+
+User = get_user_model()
+
+
+class UserProfileDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class UpdateProfileView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = ProfileSerializer
+    lookup_url_kwarg = ("username",)
+
+
+class ListFreelancers(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
